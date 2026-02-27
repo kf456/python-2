ba615cad71dbe4ece55c9838c5eef709530603d9
kf456 <kf893136@gmail.com>
2026-02-27 14:08:52 +0500

    command complete

---
+++ input output and operations.py
@@ -0,0 +1,29 @@
+#variable
+#datatypes
+#functions: builtin function print(),type()
+
+# add two values.
+
+num1 = 780
+num2 = 690
+add = num1 + num2
+res = add/2
+#print(num1)
+#print(add)
+#print(res)
+
+
+input()
+#input("Enter a number of your choice:")
+#user_input = input("Enter a number of your choice:")
+
+ value= istr-valu,"98"
+int_valu = int(valu)
+print(type(int_valu))
+print(type(valu))
+
+#num1 = int(input("enter first number to add"))
+#num2 = int(input("enter second number to add"))
+#add = num1 + num2
+#print(num1,num2,add)
+#print(type(num1))
