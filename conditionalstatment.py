ab82bd8a8157d72b2cb6fc93b21f9632d5d8b88d
kf456 <kf893136@gmail.com>
2026-02-27 13:31:31 +0500

    challenge

---
+++ conditionalstatment.py
@@ -0,0 +1,15 @@
+if 5 > 20:
+    print("50 is greater than 20")
+    print("we are now inside the if block")
+    
+# write a program to show marks and find its percentage 
+# say congratualtion if marks are above 60.
+marks = float(input("enter your marks:"))
+
+if marks > 60:
+    print("congratulations you're passed", marks)
+    percetange = marks/100*100
+    print("your percentage is:",percetange, "%" )
+
+
+
