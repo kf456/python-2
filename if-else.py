6ec6b878a2f2dbae5feb27fe6140db36f2543642
kf456 <kf893136@gmail.com>
2026-03-02 13:40:36 +0500

    demand

---
+++ if-else.py
@@ -0,0 +1,20 @@
+#age= float(input("enter a nmber of your choice"))
+#if age >= 15:
+   # print("you are eilgible for vote")
+#else:
+   # print("sorry")
+
+#age = 12
+
+#if age >= 18:
+    #print("You are an adult.")
+#else:
+  #  print("You are a minor.")
+
+temperature = 45
+is_sunny = True
+
+if temperature > 55 and is_sunny:
+    print("Great day for the beach!")
+else:
+    print("Maybe stay inside.")
